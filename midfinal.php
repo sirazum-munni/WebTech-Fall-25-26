525e4b01d833ad4bacc9675227e79d4f95eb7ad9
sirazum-munni <munnikhan.1221@gmail.com>
2025-11-16 12:15:27 +0600

    Add files via upload

---
+++ midfinal.php
@@ -0,0 +1,39 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title> Form Handler </title>
+</head>
+<body style="background_color.aquamarine;">
+
+<center>
+    <h1> Participent Registration </h1>
+    <table>
+        <tr>
+            <td> Full Name: </td>
+            <td> <input type= "text"> </td>
+</tr>
+<tr>
+    <td> Email: </td>
+    <td><input type= "text"> </td>
+</tr>
+<tr>
+    <td>Phone Number:</td>
+    <td><input type="Number"></td>
+</tr>
+<tr>
+    <td> Password: </td>
+    <td> <input type ="text"></td>
+</tr>
+<tr>
+    <td style="padding;10px;"> Confirm password:</td>
+<td><input type="text">
+</td>
+</tr>
+<tr>
+    <td><input type="submit"></td>
+</tr>
+</table>
+</center>
+<center>
+<h2> Registor</h2>
+</center>
